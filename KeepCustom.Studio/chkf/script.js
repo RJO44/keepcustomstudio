98cf2edd64768a0f1f94ffe77af856e54699b190
KeepCustom_Studio <rj.officer44@gmail.com>
2026-01-13 11:05:43 +0700

    Add files via upload

---
+++ KeepCustom.Studio/chkf/script.js
@@ -0,0 +1,4 @@
+document.getElementById('backBtn').addEventListener('click', function(){
+  // Ganti URL berikut dengan tautan web utama kamu
+  window.location.href = 'https://www.example.com';
+});
